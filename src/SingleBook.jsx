72a6e41efd5f1e1f0df83c3b5afa4424377201b4
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-09-07 18:11:26 +0200

    09-D4-x

--- src/SingleBook.jsx
+++ src/SingleBook.jsx
@@ -1,20 +1,28 @@
 import { Card } from "react-bootstrap";
-// import CommentArea from "./data/CommentArea";
+import CommentArea from "./data/CommentArea";
 import { Component } from "react";
 
 class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
   render() {
     return (
       <div className="col-12 col-md-6 col-lg-4 gy-5 ">
-        <Card className="m-auto">
+        <Card
+          onClick={() => this.setState({ selected: !this.state.selected })}
+          style={{ border: this.state.selected ? "3px solid red" : "none" }}
+          className="m-auto"
+        >
           <Card.Body>
             <Card.Img style={{ height: "300px", objectFit: "contain" }} variant="top" src={this.props.img} />
             <Card.Title className="fs-6">{this.props.title}</Card.Title>
-            <Card.Title commentsprops={this.commentsprops} />
+            {this.state.selected && <CommentArea asin={this.props.book.asin} />}
           </Card.Body>
         </Card>
       </div>
     );
   }
 }
+
 export default SingleBook;
--- src/data/CommentArea.jsx
+++ src/data/CommentArea.jsx
@@ -1,35 +1,48 @@
-// import { Component } from "react";
-// // import { Card } from "react-bootstrap";
+import { Component } from "react";
+import { Card } from "react-bootstrap";
+// import { Card } from "react-bootstrap";
 
-// class CommentArea extends Component {
-//   state = {
-//     comments: [],
-//     selected: false,
-//   };
-//   fetchBooks = async () => {
-//     const url = "https://striveschool-api.herokuapp.com/api/comments/";
-//     try {
-//       //   console.log(this.state.commentsprops.book.asin);
-//       const response = await fetch(url + this.props.commentsprops.book.asin, {
-//         headers: {
-//           Authorization:
-//             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjE1NGMwMzRmZjAwMTQwM2Y1MzMiLCJpYXQiOjE2OTQwOTExNzEsImV4cCI6MTY5NTMwMDc3MX0.4iQCgMChdawJVCHs990aZB4de6Zn3GqodkDeRIIi9bM",
-//         },
-//       });
-//       if (response.ok) {
-//         const data = await response.json();
-//         this.setState({ comments:  });
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   componentDidMount = () => {
-//     console.log("didMount");
-//     this.fetchBooks();
-//   };
-//   render() {
-//     return <div>{/* <Card.Title className="fs-6">{Comments}</Card.Title> */}ciao</div>;
-//   }
-// }
-// export default CommentArea;
+class CommentArea extends Component {
+  state = {
+    comments: [],
+    selected: false,
+  };
+
+  fetchBooks = async () => {
+    const url = "https://striveschool-api.herokuapp.com/api/comments/";
+    try {
+      //   console.log(this.state.commentsprops.book.asin);
+      const response = await fetch(url + this.props.asin, {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjE1NGMwMzRmZjAwMTQwM2Y1MzMiLCJpYXQiOjE2OTQwOTExNzEsImV4cCI6MTY5NTMwMDc3MX0.4iQCgMChdawJVCHs990aZB4de6Zn3GqodkDeRIIi9bM",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        this.setState({ comments: data });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  componentDidMount = () => {
+    console.log("didMount");
+    this.fetchBooks();
+  };
+  render() {
+    return (
+      <Card.Title className="fs-6">
+        {this.state.comments.map((comment) => (
+          <div>
+            <p>{comment.author}</p>
+            <p>{comment.comment}</p>
+            <p>{comment.rate}</p>
+          </div>
+        ))}
+      </Card.Title>
+    );
+  }
+}
+
+export default CommentArea;
